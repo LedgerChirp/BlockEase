e0b0d613f72fe03bfcb1ea822db20c93ce524359
Mustafa-Azad <mustafaazad533@gmail.com>
2023-02-15 18:20:31 +0530

    transfer update

--- components/Landing/Navbar.tsx
+++ components/Landing/Navbar.tsx
@@ -12,7 +12,7 @@ function Navbar() {
 			<div className="fixed w-[80%] mx-auto h-16 flex justify-center">
 				<div className="flex items-center justify-evenly">
 					<div className="">
-						<Image src={"/vercel.svg"} width={100} height={100} />
+						<Image src={"/vercel.svg"} width={100} height={100} alt="logo" />
 					</div>
 					<div className="space-x-6 flex ml-28">
 						{nav.map((item, index) => (
