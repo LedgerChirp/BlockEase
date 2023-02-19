87c6d4c75ac21b225b762ee256734b9190b26113
Kanishk Tiwari <72643990+kanishk6103@users.noreply.github.com>
2023-02-19 14:40:41 +0530

    Update transfer.tsx

--- pages/transfer.tsx
+++ pages/transfer.tsx
@@ -7,7 +7,7 @@ import Convertor from "../components/Home/Converter";
 function transfer() {
 	return (
 		<>
-			<div className="grid gap-8 grid-cols-[0.2fr_26%_auto] grid-rows-[0.7fr_0.3fr] gap-[0px_0px] grid-flow-row m-5 mt-8">
+			<div className="grid gap-6 grid-cols-[0.2fr_26%_auto] grid-rows-[0.7fr_0.3fr] gap-[0px_0px] grid-flow-row m-5 mt-8">
 				<Sidebar />
 				<QR />
 				<SendMoney/>
